f03f74246d195dde41bfb1f5b1e8568a578531d8
Neven <neven.vuger@gmail.com>
2016-01-27 11:53:00 +0100

    Added side menu for smaller screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,12 @@ $(window).scroll(function() {
     if (scroll >= 70) {
         $(".main-nav").addClass("scroll");
     }
-    // if (scroll >= 400) {
-    //     $(".navbar-public").removeClass("navbar-transparent");
-    // }
-    // if (scroll < 400) {
-    //     $(".navbar-public").addClass("navbar-transparent");
-    // }
+    if (scroll >= 400) {
+        $(".main-nav").removeClass("translate-right");
+    }
+    if (scroll < 400) {
+        $(".main-nav").addClass("translate-right");
+    }
 });
 
 //Tabs on landing
@@ -45,16 +45,15 @@ $(document).ready(function () {
     $(this).addClass('current'); //add current class on clicked tab title
   });
 
-
-
-
-
-
+  //change call to action link on small screens
+  if ($(window).width() < 730) {
+    $(".cta__primary--big, .cta__primary--sm").attr("href", "https://www.vibby.com/explore/");
+  }
 });
 
-
+//mobile side menu
 function addShowRightMenu(elem) {
   elem.classList.toggle('menu-icon--active');
-  var mainNav = document.querySelector('.main-nav');
+  var mainNav = document.querySelector('.navs');
   mainNav.classList.toggle('mobile-menu');
 }
