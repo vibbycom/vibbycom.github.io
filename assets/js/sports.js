336f91307f3d2535c5df4bc796fd69f70b9b1c35
Matej <matejserbec@Matejs-MBP.lan>
2016-01-29 19:11:01 +0100

    random sport background

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -4,12 +4,15 @@ var indexBefore = 0;
 var totalNumberOfBackgroundImages = 7;
 
 $(document).ready(function () {
-  var heroElem = $('#hero');
-  tabsInterval = setInterval(function(){
-    heroElem.toggleClass('bg' + indexAfter);
-    heroElem.toggleClass('bg' + indexBefore);
-    indexBefore = indexAfter;
-    indexAfter++;
-    indexAfter %= totalNumberOfBackgroundImages;
-  }, 10000);
+
+  var random = parseInt(Math.random() * 10 & 7);
+  $('#hero').toggleClass('bg' + random);
+
+  //tabsInterval = setInterval(function(){
+  //  heroElem.toggleClass('bg' + indexAfter);
+  //  heroElem.toggleClass('bg' + indexBefore);
+  //  indexBefore = indexAfter;
+  //  indexAfter++;
+  //  indexAfter %= totalNumberOfBackgroundImages;
+  //}, 10000);
 });
