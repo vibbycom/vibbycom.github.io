c44bf12bc05036cacde04e864cc156e82849287e
Neven <neven.vuger@gmail.com>
2017-02-03 13:05:49 +0100

    Video playback starts and stops depending on focus

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -6,7 +6,6 @@ var contents; //grab contents
 var currentIndex = 0;
 var totalNumberOfFeatureTabs = 3;
 
-
 //Tabs on landing
 $(document).ready(function () {
 
@@ -80,6 +79,30 @@ $(document).ready(function () {
     clearInterval(tabsInterval);
     switchTabContent($(this).index());
   });
+
+  // Get media - with autoplay disabled (audio or video)
+  var media = $('video');
+  var tolerancePixel = 80;
+
+  function checkMedia(){
+      // Get current browser top and bottom
+      var scrollTop = $(window).scrollTop() + tolerancePixel;
+      var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+      media.each(function(index, el) {
+          var yTopMedia = $(this).offset().top;
+          var yBottomMedia = $(this).height() + yTopMedia;
+
+          if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
+              $(this).get(0).play();
+          } else {
+              $(this).get(0).pause();
+          }
+      });
+
+      //}
+  }
+  $(document).on('scroll', checkMedia);
 
 });
 
