67d3bc6dbb523792d8708029d8921cadf2c5fa5f
Neven <neven.vuger@gmail.com>
2018-02-13 16:16:34 +0100

    disabled scroll play/pause video on landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,34 +181,34 @@ $('a[href*="#"]')
   });
 
   // Get media - with autoplay disabled (audio or video)
-  var media = $('video');
-  var tolerancePixel = 80;
+  // var media = $('video');
+  // var tolerancePixel = 80;
 
-  function checkMedia(){
-      // Get current browser top and bottom
-      var scrollTop = $(window).scrollTop() + tolerancePixel;
-      var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+  // function checkMedia(){
+  //     // Get current browser top and bottom
+  //     var scrollTop = $(window).scrollTop() + tolerancePixel;
+  //     var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
 
-      media.each(function(index, el) {
-          var yTopMedia = $(this).offset().top;
-          var yBottomMedia = $(this).height() + yTopMedia;
+  //     media.each(function(index, el) {
+  //         var yTopMedia = $(this).offset().top;
+  //         var yBottomMedia = $(this).height() + yTopMedia;
 
-          if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
-              if ($(this).attr('id') === "player-container-bc_html5_api") {
-                if (window.currentVibbifyState) {
-                  $(this).get(0).play();
-                }
-              } else {
-                $(this).get(0).play();
-              }
-          } else {
-              $(this).get(0).pause();
-          }
-      });
+  //         if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
+  //             if ($(this).attr('id') === "player-container-bc_html5_api") {
+  //               if (window.currentVibbifyState) {
+  //                 $(this).get(0).play();
+  //               }
+  //             } else {
+  //               $(this).get(0).play();
+  //             }
+  //         } else {
+  //             $(this).get(0).pause();
+  //         }
+  //     });
 
-      //}
-  }
-  $(document).on('scroll', checkMedia);
+  //     //}
+  // }
+  // $(document).on('scroll', checkMedia);
   
 });
 
